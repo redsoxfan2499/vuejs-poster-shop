bf8367aecff978d3a6c402c8145cbacb2bddeef0
dladner <dxl180015@utdallas.edu>
2018-10-09 17:43:45 -0500

    added quantity functionality

--- public/script.js
+++ public/script.js
@@ -1,3 +1,4 @@
+var PRICE = 9.99;
 new Vue({
     el: '#app',
     data: {
@@ -5,25 +6,69 @@ new Vue({
         total: 0,
         items: [
             {
-                title: 'Item 1',
-            }, {
-                title: 'Item 2',
-            }, {
-                title: 'Item 3',
+                id: 1,
+                title: 'Item 1'              
+            }, 
+            {
+                id: 2,
+                title: 'Item 2'             
+            }, 
+            {
+                id: 3,
+                title: 'Item 3'
             }
         ],
-        cart: []
+        cart: [],
+        search: ''
     },
     methods: {
+        onSubmit: function(){
+            console.log(this.$http);
+        },
         addItem: function (index) {
             this.total += 9.99;
             var item = this.items[index];
-            this.cart.push({
-                title: item.title,
-                qty: 1
-            });
+            var found = false;
+            for ( var i = 0; i < this.cart.length; i++) {
+                if(this.cart[i].id === item.id) {
+                    found = true;
+                    this.cart[i].qty++;
+                    break;
+                }
+            }
+
+            if(!found) {
+                this.cart.push({
+                    id: item.id,
+                    title: item.title,
+                    qty: 1,
+                    price: PRICE
+                });
+            }
+            
 
 
+        },
+        inc: function(item){
+            item.qty++;
+            this.total += PRICE;    
+        },
+        dec: function(item) {
+            item.qty--;
+            this.total -= PRICE;
+            if( item.qty <= 0 ) {
+                for ( var i = 0; i < this.cart.length; i++) {
+                    if(this.cart[i].id === item.id) {
+                        this.cart.splice(i, 1);
+                        break;
+                    }
+                }
+            } 
+        }
+    },
+    filters: {
+        currency: function(price) {
+           return '$' .concat(price.toFixed(2));
         }
     }
 });
