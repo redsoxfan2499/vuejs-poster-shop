73b4e13bc065ceafff38f2e64ac205960ee746b0
Darren Ladner <dxladner@gmail.com>
2018-10-08 20:05:53 -0500

    added javascript to script.js file

--- public/script.js
+++ public/script.js
@@ -1,1 +1,21 @@
-console.log('It works.');
+new Vue({
+    el: '#app',
+    data: {
+        message: 'test',
+        total: 0,
+        items: [
+            {
+                title: 'Item one',
+            }, {
+                title: 'Item two',
+            }, {
+                title: 'Item three',
+            }
+        ]
+    },
+    methods: {
+        addItem: function () {
+            this.total += 9.99;
+        }
+    }
+});
